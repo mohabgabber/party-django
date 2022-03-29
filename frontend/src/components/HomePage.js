a108a3d19c548600a0042ef416eb111ce228eefe
Mohab Gabber <mohabgabber1@protonmail.com>
2022-03-29 20:58:59 +0200

    initial commit

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -19,8 +19,8 @@ export default class HomePage extends Component {
       <Router>
         <Routes>
           <Route exact path="/"><p>This is the home page</p></Route>
-          <Route path="/join" component={RoomJoinPage} />
-          <Route path="/create" component={CreateRoomPage} />
+          <Route path="/join" element={<RoomJoinPage />} />
+          <Route path="/create" element={<CreateRoomPage />} />
         </Routes>
       </Router>
     );
