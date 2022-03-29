4c817b8974cf973ffdc067b317c543f403d3800f
Mohab Gabber <mohabgabber1@protonmail.com>
2022-03-29 21:06:58 +0200

    removed react-router-dom v6 for v5

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -3,7 +3,7 @@ import RoomJoinPage from "./RoomJoinPage";
 import CreateRoomPage from "./CreateRoomPage";
 import {
   BrowserRouter as Router,
-  Routes,
+  Switch,
   Route,
   Link,
   Redirect,
@@ -17,11 +17,13 @@ export default class HomePage extends Component {
   render() {
     return (
       <Router>
-        <Routes>
-          <Route exact path="/"><p>This is the home page</p></Route>
-          <Route path="/join" element={<RoomJoinPage />} />
-          <Route path="/create" element={<CreateRoomPage />} />
-        </Routes>
+        <Switch>
+          <Route exact path="/">
+            <p>This is the home page</p>
+          </Route>
+          <Route path="/join" component={RoomJoinPage} />
+          <Route path="/create" component={CreateRoomPage} />
+        </Switch>
       </Router>
     );
   }
