772a461139cd7751384d4778c76fb51ad17782f5
Mohab Gabber <mohabgabber1@protonmail.com>
2022-03-30 21:39:41 +0200

    finished song pause

--- frontend/src/components/MusicPlayer.js
+++ frontend/src/components/MusicPlayer.js
@@ -8,8 +8,17 @@ export default class MusicPlayer extends Component {
     constructor(props) {
         super(props);
     }
+    pauseSong(){
+        const requestOptions = {method: "PUT", headers: {"Content-Type": "application/json"}};
+        fetch("/spotify/pause", requestOptions);
+    }
+    playSong(){
+        const requestOptions = {method: "PUT", headers: {"Content-Type": "application/json"}};
+        fetch("/spotify/play", requestOptions);
+    }
     render() {
         const songProgress = (this.props.time / this.props.duration) * 100;
+
         return (<Card>
                     <Grid container alignItems="center">
                         <Grid item align="center" xs={4}>
@@ -23,16 +32,16 @@ export default class MusicPlayer extends Component {
                                 {this.props.artist}
                             </Typography>
                             <div>
-                                <IconButton>
-                                    { this.props.is_playing ? <PauseIcon /> : <PlayArrowIcon /> }
-                                </IconButton>
-                                <IconButton>
-                                    <SkipNextIcon />
-                                </IconButton>
+                            <IconButton onClick={() => {this.props.is_playing ? this.pauseSong() : this.playSong();}}>
+                                { this.props.is_playing ? <PauseIcon /> : <PlayArrowIcon /> }
+                            </IconButton>
+                            <IconButton>
+                                <SkipNextIcon />
+                            </IconButton>
                             </div>
                         </Grid>
                     </Grid>
-                    <LinearProgress variant='determinate' value={songProgress} />
+                    <LinearProgress variant="determinate" value={songProgress} />
                 </Card>);
     }
 }
--- spotify/models.py
+++ spotify/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class SpotifyToken(models.Model):
     user = models.CharField(max_length=100, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    refresh_token = models.CharField(max_length=200, null=True)
+    refresh_token = models.CharField(max_length=200)
     access_token = models.CharField(max_length=200)
     expires_in = models.DateTimeField()
     token_type = models.CharField(max_length=50)
